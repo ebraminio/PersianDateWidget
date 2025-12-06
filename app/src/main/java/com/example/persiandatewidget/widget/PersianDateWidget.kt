c513b668e746c4c4a7661c0d6bb0821cf2dda596
Ebrahim Byagowi <ebrahim@gnu.org>
2025-12-06 12:24:22 +0330

    Apply clip and padding to the ripple effect

--- app/src/main/java/com/example/persiandatewidget/widget/PersianDateWidget.kt
+++ app/src/main/java/com/example/persiandatewidget/widget/PersianDateWidget.kt
@@ -42,68 +42,68 @@ class PersianDateWidget : GlanceAppWidget() {
         val useColorful = WidgetPreferences.isColorful(dataStore)
         val backgroundAlpha = WidgetPreferences.getBackgroundAlpha(dataStore)
         val cornerRadius = WidgetPreferences.getCornerRadius(dataStore)
-        val padding = WidgetPreferences.getPadding(dataStore)
-
-        AndroidRemoteViews(
-            RemoteViews(context.packageName, R.layout.widget_backround).also {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) it.setFloat(
-                    R.id.widget_content,
-                    "setAlpha",
-                    if (useColorful) backgroundAlpha else 1f
-                )
-            },
-            R.id.widget_content,
-            GlanceModifier
-                .fillMaxSize()
-                .padding(vertical = padding.dp),
-        ) {
-            Box(
-                GlanceModifier
-                    .cornerRadius(cornerRadius.dp)
-                    .background(
-                        if (useColorful) GlanceTheme.colors.widgetBackground else ColorProvider(
-                            day = Color.White.copy(alpha = backgroundAlpha),
-                            night = Color.Black.copy(alpha = backgroundAlpha)
-                        )
+        val padding = WidgetPreferences.getPadding(dataStore).let { if (it == 0f) 0f else it + .5f }
+        Box(GlanceModifier.padding(vertical = padding.dp)) {
+            AndroidRemoteViews(
+                RemoteViews(context.packageName, R.layout.widget_backround).also {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) it.setFloat(
+                        R.id.widget_content,
+                        "setAlpha",
+                        if (useColorful) backgroundAlpha else 1f
                     )
-                    .fillMaxSize(),
-            ) {}
-        }
-        Column(
-            modifier = GlanceModifier
-                .fillMaxSize()
-                .padding(4.dp)
-                .clickable(onClick = actionRunCallback<OpenCalendarAction>()),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            val persianDate = PersianDate()
-            Text(
-                text = persianDate.dayOfMonth,
-                style = TextStyle(
-                    color = if (useColorful) {
-                        GlanceTheme.colors.primary
-                    } else {
-                        GlanceTheme.colors.onBackground
-                    },
-                    fontSize = 34.sp,
-                    fontWeight = FontWeight.Bold,
-                ),
-            )
+                },
+                R.id.widget_content,
+                GlanceModifier.fillMaxSize(),
+            ) {
+                Box(
+                    GlanceModifier
+                        .cornerRadius(cornerRadius.dp)
+                        .background(
+                            if (useColorful) GlanceTheme.colors.widgetBackground else ColorProvider(
+                                day = Color.White.copy(alpha = backgroundAlpha),
+                                night = Color.Black.copy(alpha = backgroundAlpha)
+                            )
+                        )
+                        .fillMaxSize(),
+                ) {}
+                // Don't bring below here as that causes alpha gets applied to the foreground as well
+            }
+            Column(
+                modifier = GlanceModifier
+                    .fillMaxSize()
+                    .clickable(onClick = actionRunCallback<OpenCalendarAction>())
+                    .cornerRadius(cornerRadius.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                val persianDate = PersianDate()
+                Text(
+                    text = persianDate.dayOfMonth,
+                    style = TextStyle(
+                        color = if (useColorful) {
+                            GlanceTheme.colors.primary
+                        } else {
+                            GlanceTheme.colors.onBackground
+                        },
+                        fontSize = 34.sp,
+                        fontWeight = FontWeight.Bold,
+                    ),
+                )
 
-            Text(
-                text = persianDate.monthName,
-                style = TextStyle(
-                    color = if (useColorful) {
-                        GlanceTheme.colors.onSurfaceVariant
-                    } else {
-                        GlanceTheme.colors.onBackground
-                    },
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.Normal,
-                ),
-                modifier = GlanceModifier.padding(top = 2.dp),
-            )
+                Text(
+                    text = persianDate.monthName,
+                    style = TextStyle(
+                        color = if (useColorful) {
+                            GlanceTheme.colors.onSurfaceVariant
+                        } else {
+                            GlanceTheme.colors.onBackground
+                        },
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Normal,
+                    ),
+                    modifier = GlanceModifier.padding(top = 2.dp),
+                )
+            }
         }
     }
 }
